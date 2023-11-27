58e54a4e2b2d2abe2ef0451fd3e6f55ec0ca1831
JOHNSON-GM <gichukijohnson93@gmail.com>
2023-11-27 22:42:19 +0300

    Done styling the pages

--- index.mjs
+++ index.mjs
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import fs from 'fs';
 import open from 'open';
+import path from 'path';
 
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 
@@ -46,13 +48,6 @@ app.post('/submit', (req, res) => {
       console.log("User already exists");
   }
 
-  // const bcrypt = require('bcrypt');
-
-  // // Hash the user's password before storing it
-  // const hashedPassword = bcrypt.hashSync(userData.password, 10);
-  // userData.password = hashedPassword;
-
-
   // Convert the updated data back to JSON string
   const updatedData = JSON.stringify(jsonData, null, 2);
 
@@ -93,11 +88,135 @@ app.post('/adminLogin', (req, res) => {
 
   if (user) {
     // Redirect to the homepage if the user is found
-    res.redirect('/home.html');
+    res.redirect('/adminPage.html');
   } else {
     // Handle authentication failure,  show an error message
     res.send('Authentication failed. Please check your email and password.');
   }
+});
+
+app.post('/add', (req, res) => {
+  const productData = {
+    ID: req.body.productId,
+    Name: req.body.name,
+    Price: req.body.price,
+    Quantity: req.body.quantity
+};
+
+
+  // Read existing JSON data from the file
+  let jsonData = [];
+  try {
+    const existingData = fs.readFileSync('data.json', 'utf-8');
+    try {
+      jsonData = JSON.parse(existingData);
+    } catch (parseError) {
+      console.log(parseError)
+      // Handle the parsing error, e.g., by initializing jsonData to an empty array
+      jsonData = [];
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  // Check if product exists and if product exists do not duplicate user
+  const existingProduct = jsonData.Products.find((Product) => Product.Name === productData.Name);
+
+  if (existingProduct) {
+    res.status(400).send('Product already exists');
+    console.log('Received data:', req.body);
+    console.log('Existing product:', existingProduct);
+
+  } else {
+    // Add the product input data to the JSON array
+    jsonData.Products.push(productData);
+
+    // Display in the console "product added successfully"
+    res.send('Product added successfully');
+
+    // Convert the updated data back to JSON string
+    const updatedData = JSON.stringify(jsonData, null, 2);
+    // Write the updated data to the JSON file
+    fs.writeFileSync('data.json', updatedData);
+  }
+});
+
+
+app.post('/post_sale', (req, res) => {
+  const saleData = {
+    Customer: req.body.customerName,
+    productID: req.body.productId,
+    Name: req.body.name,
+    Price: req.body.price,
+    Quantity: req.body.quantity,
+    Date_of_sale: req.body.date
+};
+
+
+  // Read existing JSON data from the file
+  let jsonData = [];
+  try {
+    const existingData = fs.readFileSync('data.json', 'utf-8');
+    try {
+      jsonData = JSON.parse(existingData);
+    } catch (parseError) {
+      console.log(parseError)
+      // Handle the parsing error, e.g., by initializing jsonData to an empty array
+      jsonData = [];
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+
+    // Add the product input data to the JSON array
+    jsonData.Sales.push(saleData);
+
+    // Display in the console "product added successfully"
+    res.send('Sale added successfully');
+
+    // Convert the updated data back to JSON string
+    const updatedData = JSON.stringify(jsonData, null, 2);
+    // Write the updated data to the JSON file
+    fs.writeFileSync('data.json', updatedData);
+  }
+);
+
+
+app.get('/fetchProducts', (req, res) => {
+  const dataPath = path.join('data.json');
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading data.json:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      const products = jsonData.Products; // Access the "products" dataset
+      res.json(products);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
+app.get('/fetchSales', (req, res) => {
+  const dataPath = path.join('data.json');
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading data.json:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      const sales = jsonData.Sales; // Access the "products" dataset
+      res.json(sales);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 });
 
 
